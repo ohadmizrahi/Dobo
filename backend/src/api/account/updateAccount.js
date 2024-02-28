dee96d75b4da315b243f1f0696312a07f069f9ad
ohad mizrahi <nmizra7@gmail.com>
2024-02-28 16:41:22 +0200

    finishing

--- backend/src/api/account/updateAccount.js
+++ backend/src/api/account/updateAccount.js
@@ -1,14 +1,15 @@
-const { find, update } = require('@src/models/account.js')
-const { validateSchema } = require('@src/utils/schema.js')
+const { update: updateAccount } = require('../../models/account.js')
+const { update: updatePaymentMethod } = require('../../models/paymentMethods.js')
+const { validateSchema } = require('../../utils/schema.js')
 
-async function updateAccount(username, fieldsToUpdate) {
+async function updateAccountDetails(username, fieldsToUpdate) {
     const schema = ["fullName", "phoneNumber", "address", "birthDate"];
     const invalidFields = validateSchema(schema, fieldsToUpdate);
     if (invalidFields.length) {
         return { success: false, invalidFields, message: 'Invalid fields to update' };
     }
 
-    const response = await update(username, fieldsToUpdate);
+    const response = await updateAccount(username, fieldsToUpdate);
     if (!response.success) {
         throw new Error(response.message);
     }
@@ -23,7 +24,42 @@ async function resetPassword(username, password) {
     return { success: true, updatedFields: { password: "*********" }, message: "Account updated"}
 }
 
+async function updatedPaymentMethod(username, fieldsToUpdate) {
+    const schema = ["cardNumber", "expiritionDate", "cvv", "citizenId", "type"];
+    const invalidFields = validateSchema(schema, fieldsToUpdate);
+    if (invalidFields.length) {
+        return { success: false, invalidFields, message: 'Invalid fields to update' };
+    }
+
+    const response = await updatePaymentMethod(username, fieldsToUpdate);
+    if (!response.success) {
+        throw new Error(response.message);
+    }
+    const secureResponse = {
+        cardNumber: "************" + response.updatedFields.cardNumber.slice(-4),
+        expiritionDate: response.updatedFields.expiritionDate,
+        cvv: "***",
+        citizenId: "********",
+        type: response.updatedFields.type
+    }
+    Object.keys(response.updatedFields).forEach(key => {
+        response.updatedFields[key] = secureResponse[key];
+    });
+    return { success: true, updatedFields: response.updatedFields, message: "Payment Method updated"}
+}
+
+async function updateImage(username, image) {
+
+    const response = await updateAccount(username, { image: image });
+    if (!response.success) {
+        throw new Error(response.message);
+    }
+    return { success: true, updatedFields: response.updatedFields, message: "Account updated"}
+}
+
 module.exports = {
-    updateAccount,
-    resetPassword
+    updateAccountDetails,
+    resetPassword,
+    updatedPaymentMethod,
+    updateImage
 }
--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -2,9 +2,9 @@ const { Router } = require('express');
 const multer = require('multer');
 const { authenticateUserToken } = require("@src/middlewares/authenticateUserToken.js");
 const { authenticateResetPasswordToken } = require("@src/middlewares/authenticateResetPasswordToken.js");
-const { find: findUserAccount, update: updateUserAccount } = require('@src/models/account.js');
-const { find: getUserPaymentMethod, update: updateUserPaymentMethod } = require('@src/models/paymentMethods.js')
-const { updateAccount, resetPassword } = require('@src/api/account/updateAccount.js');
+const { find: findUserAccount } = require('@src/models/account.js');
+const { find: getUserPaymentMethod } = require('@src/models/paymentMethods.js')
+const { updateAccountDetails, resetPassword, updatedPaymentMethod, updateImage } = require('@src/api/account/updateAccount.js');
 
 const router = Router();
 
@@ -45,7 +45,7 @@ router.post("/api/profile/update/account", async (req, res) => {
     const username = req.user.username;
     const fieldsToUpdate = req.body;
     try {
-        const response = await updateAccount(username, fieldsToUpdate)
+        const response = await updateAccountDetails(username, fieldsToUpdate)
         if (!response.success) {
             res.status(400).json({ error: response.message, invalidFields: response.invalidFields});
         } else {
@@ -66,16 +66,20 @@ router.post("/api/profile/update/payment-method", async (req, res) => {
     const username = req.user.username;
     const fieldsToUpdate = req.body;
     try {
-        const paymentMethod = await getUserPaymentMethod(username);
-        if (paymentMethod) {
-            const updatedPaymentMethod = await updateUserPaymentMethod(username, fieldsToUpdate);
-            res.status(200).json({ success: true, data: updatedPaymentMethod });
+        const response = await updatedPaymentMethod(username, fieldsToUpdate)
+        if (!response.success) {
+            res.status(400).json({ error: response.message, invalidFields: response.invalidFields});
         } else {
-            res.status(404).json({ error: 'Payment Method not found' });
+            res.status(200).json({ success: true, updatedFields: response.updatedFields });
         }
-    } catch (error) {
+    }
+    catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred during updating payment method.' });
+        if (error.message === 'Payment Method not found') {
+            res.status(404).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'An error occurred during updating payment method.' });
+        }
     }
 });
 
@@ -102,16 +106,20 @@ router.post("/api/profile/update/image", upload.single('image'), async (req, res
     const username = req.user.username;
     const filename = req.file.filename;
     try {
-        const account = await findUserAccount(username);
-        if (account) {
-            const updatedAccount = await updateUserAccount(username, { image: filename});
-            res.status(200).json({success: true, data: updatedAccount });
+        const response = await updateImage(username, filename)
+        if (!response.success) {
+            res.status(400).json({ error: response.message, invalidFields: response.invalidFields});
         } else {
-            res.status(404).json({ error: 'Account not found' });
+            res.status(200).json({ success: true, updatedFields: response.updatedFields });
         }
-    } catch (error) {
+    }
+    catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred during updating image.' });
+        if (error.message === 'Account not found') {
+            res.status(404).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'An error occurred during updating account.' });
+        }
     }
 });
 
