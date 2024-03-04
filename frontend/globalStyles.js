a3904dedbfd062720e165dd5bb967ffb7f1b1ff5
orsolomon <orsolomon24@gmail.com>
2024-03-04 18:19:32 +0200

    new files

---
+++ frontend/globalStyles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#3D3D3D",
+        padding: 20,
+    },
+    text: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    image: {
+        width: 400,
+        height: 200,
+    },
+    button: {
+        backgroundColor: "#fff",
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 20,
+        width: 200,
+    },
+    buttonText: {
+        color: "#000",
+        fontSize: 20,
+        textAlign: "center",
+    },
+    });
+    
