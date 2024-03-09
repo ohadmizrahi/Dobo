b477ae447641991e215cd63ceac2c089591fa68b
danb24 <danuri240595@gmail.com>
2024-03-10 00:22:22 +0200

    update pages and components

--- frontend/components/NewAccount.js
+++ frontend/components/NewAccount.js
@@ -1,1 +1,29 @@
 // create account btn
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'; // Assuming you're using react-navigation
+import { globalStyles } from '../globalStyles';
+
+const DontHaveAccount = ({ navigation }) => (
+  <View style={globalStyles.formHeadLine}>
+    <Text style={styles.text}>Don't have an account? </Text>
+    <TouchableOpacity onPress={() => { navigation.navigate('SignUp')}}>
+      <Text style={styles.link}>Click here to create</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row', // Arrange text and link horizontally
+    alignItems: 'center', // Align text and link vertically
+  },
+  text: {
+    fontSize: 16,
+  },
+  link: {
+    color: '#0000EE', // Change to your desired link color
+    fontWeight: 'bold',
+  },
+});
+
+export default DontHaveAccount;
--- frontend/components/SocialSignIn.js
+++ frontend/components/SocialSignIn.js
@@ -1,25 +1,17 @@
 // google and facebook sing in section ( sign in & sign up screens)
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text,Button } from 'react-native';
 import { globalStyles } from '../globalStyles'
 
-const SocialSignInButton = ({ provider, onPress }) => (
-  <TouchableOpacity style={globalStyles.buttonSocial} onPress={() => onPress(provider)}>
-    <Text style={globalStyles.buttonText}>{`Sign in with ${provider}`}</Text>
-  </TouchableOpacity>
+const SocialSignIn = () => (
+  <>
+    <Button title="Continue with Google" Style={globalStyles.SocialSignIn} titleStyle={globalStyles.socialtext}
+      onPress={() => {console.log('google test')}}
+    />
+    <Button
+      title="Continue with Facebook" Style={globalStyles.SocialSignIn} titleStyle={globalStyles.socialtext}
+      onPress={() => {console.log('facebook test')}}
+    />
+  </>
 );
-
-const SocialSignIn = () => {
-  const handleSocialLogin = (provider) => {
-    console.log(`Logging in with ${provider}`);
-  };
-
-  return (
-    <View>
-      <SocialSignInButton provider="facebook" onPress={handleSocialLogin} />
-      <SocialSignInButton provider="google" onPress={handleSocialLogin} />
-    </View>
-  );
-};
-
 export default SocialSignIn;
