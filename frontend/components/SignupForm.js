296285e1fa912fb7d49b341c91e6c8836cf3de1a
orsolomon <orsolomon24@gmail.com>
2024-03-19 21:18:31 +0200

    api in signup, need error handeling

--- frontend/components/SignupForm.js
+++ frontend/components/SignupForm.js
@@ -13,14 +13,51 @@ const SignUpForm = () => {
     { name: 'confirmPassword', label: 'Confirm Password', iconName: 'lock', placeholder: 'Confirm password', secureTextEntry: true },
   ];
 
+  const onSubmit = async (values, { resetForm }) => {
+    const stam={
+      name: values.fullName,
+      email: values.email,
+      phone: values.phoneNumber,
+      address: values.address,
+      birthday: values.birthday,
+      password: values.password,
+    }
+    const [day, month, year] = values.birthday.split("/");
+    const formattedDate = `${year}-${month}-${day}`;
+    stam["birthday"] = formattedDate;
+    try {
+      const response = await fetch('http://10.100.102.51:3000/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify(stam),
+      });
+      const data = await response.json(); // Extract JSON data from response
+      console.log('Response from server:', data);
+      // if (response.ok) {
+      //   const data = await response.json();
+      //   console.log('Response from server:', data);
+      //   resetForm();
+      // } else {
+      //   throw new Error('Error: ' + response.statusText);
+      // }
+    } catch (error) {
+      console.log("hi ohad")
+      console.error('Error:', error);
+      
+    }
+  };
+  
   return (
-<Form
-  initialValues={{ fullName: '', email: '', phoneNumber: '', address: '', birthday: '', password: '', confirmPassword: '' }}
-  validationSchema={signUpValidationSchema}
-  onSubmit={(values) => console.log(values)}
-  fields={fields}
-  submitTitle="SIGN UP" // This is a string
-/>
+    <Form
+      initialValues={{ fullName: '', email: '', phoneNumber: '', address: '', birthday: '', password: '', confirmPassword: '' }}
+      validationSchema={signUpValidationSchema}
+      onSubmit={onSubmit}
+      fields={fields}
+      submitTitle="SIGN UP"
+    />
   );
 };
 
