c6ec3f64df551eda7ffa839c975fcb116d163a60
orsolomon <orsolomon24@gmail.com>
2024-03-28 16:44:12 +0200

    item changes component ready

--- frontend/screens/ItemScreen.js
+++ frontend/screens/ItemScreen.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { View,ScrollView, Text, Button } from 'react-native';
 import {globalStyles} from '../globalStyles';
 import Bell from '../components/Bell';
+import ItemChanges from '../components/ItemChanges';
+
 export default function ItamScreen({navigation}) {   
   return (
     <ScrollView style={globalStyles.screenColor} >
-      <Bell />
+      <ItemChanges />
     </ScrollView>
   );
 }
