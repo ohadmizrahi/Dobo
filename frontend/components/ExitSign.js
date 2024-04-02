068f3e61f58d45a9d9f61cdc8215d5cd622ecf1e
orsolomon <orsolomon24@gmail.com>
2024-04-02 13:50:31 +0300

    sync

---
+++ frontend/components/ExitSign.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, TouchableOpacity , StyleSheet } from "react-native";
+import { globalStyles } from "../globalStyles";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from '@react-navigation/native'; 
+
+export default function ExitSign() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Icon name="times-circle-o" size={30} color="grey" />
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginRight: 16,
+    alignItems: 'flex-end',
+  },
+});
--- frontend/screens/ItemScreen.js
+++ frontend/screens/ItemScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View,ScrollView, Text, Button } from 'react-native';
+import { View,ScrollView, SafeAreaView, Button } from 'react-native';
 import {globalStyles} from '../globalStyles';
 import ItemChanges from '../components/ItemChanges';
 import ItemView from '../components/Itemview';
+import ExitSign from '../components/ExitSign';
 
 export default function ItamScreen({route, navigation}) {   
   return (
-    <ScrollView style={globalStyles.screenColor} >
+    <ScrollView style={globalStyles.screenColor}>
+      <ExitSign />
       <ItemChanges route={route}/>
       <ItemView navigation={navigation} route={route} />
     </ScrollView>
