38c3f9be818b2adaaba9fb8d18711793417e3b5b
orsolomon <orsolomon24@gmail.com>
2024-04-19 18:32:49 +0300

    exit sign imported to OrderScreen

--- frontend/screens/OrderScreen.js
+++ frontend/screens/OrderScreen.js
@@ -3,14 +3,14 @@ import Menu from '@Components/Menu';
 import {globalStyles} from '@Root/globalStyles';
 import LogoImage from '@Components/DoboLogo';
 import CustomButton from '@Components/CustomButton';
-
+import ExitSign from '@Components/ExitSign';
 export default function OrderScreen({ navigation }) {
   return (
     <ScrollView style={globalStyles.screenColor}>
         <ExitSign/>
         <LogoImage/>
-        <CustomButton navigation={navigation} screen='OrderCart' title={'View Order'} />
         <Menu navigation={navigation} isOrderScreen={true} />
+        <CustomButton navigation={navigation} screen='OrderCart' title={'View Order'} />
     </ScrollView>
   );
 }
