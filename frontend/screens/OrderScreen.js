483b2118dfca538400a95acb1063185514bb0cf0
barziv555 <barziv98@gmail.com>
2024-04-21 13:11:00 +0300

    i

--- frontend/screens/OrderScreen.js
+++ frontend/screens/OrderScreen.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
-import Menu from '../components/Menu';
-import { globalStyles } from '../globalStyles';
-import LogoImage from '../components/DoboLogo';
-import MoveScreenButton from '../components/MoveScreenBtn';
-import TableHeader from '../components/TableHeader';
-=======
 import { ScrollView,StatusBar } from 'react-native';
 import Menu from '@Components/Menu';
 import {globalStyles} from '@Root/globalStyles';
@@ -14,7 +5,6 @@ import LogoImage from '@Components/DoboLogo';
 import CustomButton from '@Components/CustomButton';
 import ExitSign from '@Components/ExitSign';
 
->>>>>>> master
 
 export default function OrderScreen({ navigation }) {
 
@@ -23,16 +13,6 @@ export default function OrderScreen({ navigation }) {
   }
 
   return (
-<<<<<<< HEAD
-    <View style={styles.container}>
-      <ScrollView style={globalStyles.screenColor}>
-        <LogoImage />
-        <TableHeader tableNumber={555} showPencilButton={false}/>
-        <Menu navigation={navigation} isOrderScreen={true} />
-      </ScrollView>
-      <MoveScreenButton navigation={navigation} screen='OrderCart' title={'View Order'} />
-    </View>
-=======
     <ScrollView style={globalStyles.screenColor}>
         <StatusBar barStyle="light-content" />
         {/* <ExitSign/> */}
@@ -40,7 +20,6 @@ export default function OrderScreen({ navigation }) {
         <Menu navigation={navigation} isOrderScreen={true} />
         <CustomButton handlePress={handleViewOrder} screen='OrderCart' title={'View Order'} />
     </ScrollView>
->>>>>>> master
   );
 }
 
