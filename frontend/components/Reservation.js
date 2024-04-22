5b0799694dd1e5986c9e5c6c43f2c9931f4066d5
danb24 <danuri240595@gmail.com>
2024-04-22 15:58:04 +0300

    update reservation.js

--- frontend/components/Reservation.js
+++ frontend/components/Reservation.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { TableReservationFormSchema } from '@Schemas/reservationSchema';
 import Form from '@Components/Form';
+import { useState, useEffect } from 'react';
+import { sendPostRequest } from '@Utils/request/send.js';
 
 const TableReservationForm = () => {
 
   const fields = [
-    {name: 'date', label: 'Pick date', iconName: 'calendar', placeholder: 'Reservation Date', Keyboardtype: 'date'},
+    {name: 'date', label: 'Date', iconName: 'calendar', placeholder: 'Reservation Date', Keyboardtype: 'date'},
     {name: 'tableSize', label: 'TableSize', iconName: 'user', placeholder: 'Number Of Pepole'},
     {name: 'hour', label: 'Hour', iconName: 'clock-o', placeholder: 'Reservation Hour', Keyboardtype: 'time'},
-    {name: 'preference',iconName: 'gear', label: 'Preference', placeholder: 'Preference'},
-    {name: 'specialRequest',iconName: 'gear', label: 'SpecialRequest (optional)', placeholder: 'Special Request'},
+    {name: 'preference',iconName: 'envelope', label: 'Preference', placeholder: 'Preference (optional)'},
+    {name: 'specialRequests',iconName: 'envelope', label: 'SpecialRequests (optional)', placeholder: 'Special Request (optional)'},
   ];
+  const onSubmit = async (values)=>{
+    const reservationInfo = {
+      date:values.date,
+      numOfPeople:values.tableSize,
+      time:values.hour,
+      preference:values.preference,
+      specialRequests:values.specialRequest,
+    };
+    console.log('Submitted Reservation Info:', reservationInfo);
+  };
 
   return (
     <Form
-      initialValues={{date: '',tableSize: '',hour: '',preference: '',specialRequest: '' }}
+      initialValues={{date: '',tableSize: '',hour: '',preference: '',specialRequests: '' }}
       validationSchema={TableReservationFormSchema}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={onSubmit}
       fields={fields}
       submitTitle= "Send Reservation"
       formName="Reservation" 
