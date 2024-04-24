ddde3c1deb0b3a8777740d2cb7865fd08ac93607
orsolomon <orsolomon24@gmail.com>
2024-04-24 15:35:28 +0300

    reset password works

--- frontend/components/PasswordForm.js
+++ frontend/components/PasswordForm.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import Form from './Form';
 import { useNavigation } from '@react-navigation/native';
+import { getData } from '@Utils/storage/asyncStorage';
+import { sendPostRequest } from '@Utils/request/send.js';
+import { handleResponse } from '@Utils/response/handler';
+import { Alert } from 'react-native';
+import { sendGetRequest } from '@Utils/request/send.js';
+
 
 const PasswordForm = () => {
   const [isLoading, setIsLoading] = useState(false);
-
-  const dummyPassword = '123456789'
+  const navigation = useNavigation();
+  const [dummyPassword,setdummyPassword] = useState('123456789')
 
   const fields = [
     { name: 'password', label: 'Password', iconName: 'lock', placeholder: 'Enter password', secureTextEntry: true },
@@ -14,7 +20,52 @@ const PasswordForm = () => {
   const onSubmit = async (values) => {
     setIsLoading(true);
 
+  const passwordInfo = {
+    password: dummyPassword,
   };
+  try {
+    const userToken = await getData('userToken');
+    const getResponse = await sendGetRequest('api/auth/token/reset-password', { userToken });
+    console.log('getResponse:', getResponse);
+    await handleResponse(
+      getResponse,
+      navigation,
+      async (data, error) => {
+        if (getResponse.success && getResponse.success.data.token) {
+          try {
+          const reset = {
+            resetPasswordToken : getResponse.success.data.token,
+            password: values.password,
+          }
+          const postResponse = await sendPostRequest('api/profile/update/password', reset, { userToken });
+          console.log('postResponse:', postResponse);
+          await handleResponse(
+            postResponse,
+            navigation,
+            async (data, error) => {
+              if (postResponse.success && postResponse.success.data.success) {
+                Alert.alert('Success', 'Your password has been updated successfully.');
+              }
+            }
+          )
+        } catch (error) {
+          const errorMessage = error.postResponse?.data?.error?.message || 'There was an error. Please try again.';
+          Alert.alert('Error', errorMessage);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+        navigation.navigate('Profile');
+      }
+    );
+  } catch (error) {
+    const errorMessage = error.getResponse?.data?.error?.message || 'There was an error. Please try again.';
+    Alert.alert('Error', errorMessage);
+  } finally {
+    setIsLoading(false);
+  }
+};
+
 
   return (
     <Form
