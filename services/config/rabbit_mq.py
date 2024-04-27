0097b5f60b5510988dae7a9635ce40a931fe8a0d
ohad mizrahi <nmizra7@gmail.com>
2024-04-27 19:59:43 +0300

    conenction to remote rabbitmq

--- services/config/rabbit_mq.py
+++ services/config/rabbit_mq.py
@@ -1,18 +1,17 @@
 import os
 import aio_pika
+
+from urllib.parse import quote
 from pydantic import BaseSettings
 
 class RabbitMQConfig(BaseSettings):
     broker_host: str = os.getenv('BROKER_HOST')
-    broker_port: int = int(os.getenv('BROKER_PORT', 5672))
+    broker_port: str = os.getenv('BROKER_PORT', 5671)
     broker_username: str = os.getenv('BROKER_USERNAME')
     broker_password: str = os.getenv('BROKER_PASSWORD')
 
-    class Config:
-        env_file = "../.env"
-
     async def connect(self):
-        connection_string = f"amqp://{self.broker_username}:{self.broker_password}@{self.broker_host}:{self.broker_port}/"
+        connection_string = f"amqps://{self.broker_username}:{quote(self.broker_password.encode(), safe='')}@{self.broker_host}:{self.broker_port}/"
         connection = await aio_pika.connect_robust(connection_string)
         return connection
 
